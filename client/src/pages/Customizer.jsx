9eee8d803a4239ba2e761dc98f2d7262d4fd14dd
kunal <kishujaiswal3@gmail.com>
2023-07-07 15:57:30 +0530

    backendurl changed

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,15 +53,18 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          prompt,
-        })
-      })
+      const response = await fetch(
+        "https://threed-clothong-backend.onrender.com",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
